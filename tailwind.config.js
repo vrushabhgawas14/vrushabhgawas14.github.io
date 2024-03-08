d7227553b0c0202781783ebd3353a08669fea459
Vrushabh <vrushabhgawas14@gmail.com>
2024-03-08 19:43:44 +0530

    Overall Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // For text
         primary: {
           "light": '#eeeeee',
           "black": '#000000',
         },
+        // For text hover
         hover:{
           "light": '#c8c8c8',
           "black": '#2c2c2c',
+        },
+        // For Background
+        secondary: {
+          light:"#d6daed",
+          dark:"#090012",
         }
       },
       screens: {
