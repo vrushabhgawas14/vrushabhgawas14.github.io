99c1e0b9f2a4612cf401917f5566855e9ccb52d2
Vrushabh <vrushabhgawas14@gmail.com>
2024-04-30 23:24:27 +0530

    project slider go on

--- src/components/ProjectTemplate.tsx
+++ src/components/ProjectTemplate.tsx
@@ -11,17 +11,17 @@ export const Template: React.FC<myProps> = (props) => {
   const { img, name, bio, stack, webLink, codeLink } = props;
   return (
     <>
-      <div className="w-80 border border-gray-600 p-2 m-2 rounded-lg">
+      <div className="min-w-80 sm:min-w-72 border border-gray-600 p-2 m-2 rounded-lg">
         <div>
           <img src={img} alt="" className="w-68" />
         </div>
-        <p className="text-center">{name}</p>
-        <p className="text-justify p-4">{bio}</p>
-        <div className="p-1">
+        <p className="text-center p-2 font-semibold">{name}</p>
+        <p className="text-justify p-2 h-32">{bio}</p>
+        <div className="p-2">
           <p className="font-semibold">Tech Stack: </p>
           <p className="p-2">{stack}</p>
         </div>
-        <div className="flex justify-between p-1 italic underline">
+        <div className="flex justify-between px-2 italic underline text-zinc-300">
           <a href={webLink} target="_blank">
             live link
           </a>
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -2,24 +2,33 @@ import Heading from "../components/Heading";
 import Line from "../components/Line";
 import Template from "../components/ProjectTemplate";
 import { Details } from "../constants/ProjectDetails";
-
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 export const Project = () => {
+  let slide: any = document.getElementById("slider");
+  slide?.addEventListener("wheel", (e: any) => {
+    e.preventDefault();
+    slide.style.scrollBehavior = "auto";
+    slide.scrollLeft += e.deltaY;
+  });
+
   return (
     <>
-      <main id="projects" className="py-10">
+      <main id="projects" className="py-10 w-[60%] sm:w-[90%] m-auto">
         <Heading text="Projects" />
-        <section className="flex mx-64 sm:mx-6 pt-10 overflow-scroll">
-          {Details.map((item) => (
-            <Template
-              img={item.img}
-              name={item.name}
-              bio={item.bio}
-              stack={item.stack}
-              webLink={item.webLink}
-              codeLink={item.codeLink}
-            />
-          ))}
-        </section>
+        <div id="slider" className="overflow-scroll sm:mx-6 scrolling-section">
+          <div className="flex py-5">
+            {Details.map((item) => (
+              <Template
+                img={item.img}
+                name={item.name}
+                bio={item.bio}
+                stack={item.stack}
+                webLink={item.webLink}
+                codeLink={item.codeLink}
+              />
+            ))}
+          </div>
+        </div>
       </main>
       <Line />
     </>
