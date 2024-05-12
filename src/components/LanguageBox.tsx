2b1b608b79ca4ec4ee7edbf2cdd67292fbc1a884
Vrushabh <vrushabhgawas14@gmail.com>
2024-05-12 21:43:51 +0530

    Project language-comp  bg update

--- src/components/LanguageBox.tsx
+++ src/components/LanguageBox.tsx
@@ -4,7 +4,7 @@ interface myProps {
 
 export const LanguageBox: React.FC<myProps> = (props) => {
   return (
-    <span className="px-2 bg-teal-800 dark:bg-blue-900/70 text-zinc-200 hover:bg-teal-950 dark:hover:bg-blue-900 cursor-pointer rounded-lg">
+    <span className="px-2 bg-teal-800 dark:bg-teal-900 text-zinc-200 hover:bg-teal-950 dark:hover:bg-teal-800 cursor-pointer rounded-lg">
       {props.text}
     </span>
   );
