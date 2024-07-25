cfefeb8b3d1597977cfde7c3d4d806cb5deb8ab7
Vrushabh <vrushabhgawas14@gmail.com>
2024-07-25 16:05:09 +0530

    Shift to Vercel2

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,24 +10,24 @@ import portfolio from "./projects/portfolio-white.png";
 import ageCalculator from "./projects/ageCalculator.png";
 
 // Tech Stack Images
-import javascript from "./techStack/javascript.svg";
-import java from "./techStack/java.svg";
-import react from "./techStack/react.svg";
-import python from "./techStack/python.svg";
-import bash from "./techStack/bash.svg";
-import markdown from "./techStack/markdown.svg";
-import typescript from "./techStack/typescript.svg";
+import javascript from "../assets/techStack/javascript.svg";
+import java from "../assets/techStack/java.svg";
+import react from "../assets/techStack/react.svg";
+import python from "../assets/techStack/python.svg";
+import bash from "../assets/techStack/bash.svg";
+import markdown from "../assets/techStack/markdown.svg";
+import typescript from "../assets/techStack/typescript.svg";
 
 //Tools
-import npm from "./tools/npm.svg";
-import docker from "./tools/docker.svg";
-import bootstrap from "./tools/bootstrap.svg";
-import git from "./tools/git.svg";
-import github from "./tools/github.svg";
-import mysql from "./tools/mysql.svg";
-import nodejs from "./tools/nodejs.svg";
-import tailwindcss from "./tools/tailwindcss.svg";
-import vscode from "./tools/vscode.svg";
+import npm from "../assets/tools/npm.svg";
+import docker from "../assets/tools/docker.svg";
+import bootstrap from "../assets/tools/bootstrap.svg";
+import git from "../assets/tools/git.svg";
+import github from "../assets/tools/github.svg";
+import mysql from "../assets/tools/mysql.svg";
+import nodejs from "../assets/tools/nodejs.svg";
+import tailwindcss from "../assets/tools/tailwindcss.svg";
+import vscode from "../assets/tools/vscode.svg";
 
 export const profile = myImage;
 
